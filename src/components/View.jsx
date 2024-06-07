9022174ecc164548ff97590428df4e8c8fa1f573
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 20:32:41 +0530

    VIEW COMPLETE

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 
 const View = () => {
-  return (
-    <div>View</div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">EMPLOYEE NAME</th>
+                                    <th scope="col">EMPLOYEE CODE</th>
+                                    <th scope="col">ADDRESS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">MARK</th>
+                                    <td>M1250</td>
+                                    <td>IJK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">OTTO</th>
+                                    <td>O1541</td>
+                                    <td>ERK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">MICHEAL</th>
+                                    <td>M1250</td>
+                                    <td>IJK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">ORNALD</th>
+                                    <td>O1541</td>
+                                    <td>ERK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">JACOB</th>
+                                    <td>M1250</td>
+                                    <td>IJK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">THORTON</th>
+                                    <td>O1541</td>
+                                    <td>ERK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">MIKE</th>
+                                    <td>M1250</td>
+                                    <td>IJK</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th scope="row">ORWELL</th>
+                                    <td>O1541</td>
+                                    <td>ERK</td>
+                                    
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default View
